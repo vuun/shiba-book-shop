44bea144c8c2adcd9de3898f66f4986d9888fccc
Worathon Wuttisakulchai <worathon.w@cdg.co.th>
2020-05-23 01:50:38 +0700

    update store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
+import store from '@/store';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import store from './store';
 import router from './router';
 
 Vue.config.productionTip = false;
