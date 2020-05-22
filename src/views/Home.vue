6514317dd61a5a76bc382ec16a6f6fb5bfb91327
Worathon Wuttisakulchai <worathon.w@cdg.co.th>
2020-05-23 00:36:50 +0700

    setting up default page

---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo darken-4" dark>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>Shiba Book Shop</v-toolbar-title>
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <Shelf/>
+      </v-container>
+    </v-content>
+
+    <v-footer color="indigo darken-4" app>
+      <v-spacer></v-spacer>
+
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+import Shelf from '@/components/Shelf.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Shelf,
+  },
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+};
+</script>
