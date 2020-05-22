267afeeb049991c73307508322bd72b5e6f5adad
Worathon Wuttisakulchai <worathon.w@cdg.co.th>
2020-05-23 03:01:30 +0700

    update ui and change calculate

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -5,15 +5,15 @@
         <thead>
           <tr>
             <th :id="'name'" class="text-left">Title</th>
-            <th :id="'quan'" class="text-left">Quantity</th>
-            <th :id="'price'" class="text-left">Price(THB)</th>
+            <th :id="'quan'" :width="`10%`" class="text-left">Quantity</th>
+            <th :id="'price'" :width="`25%`" class="text-left">Price</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="book in cartList" :key="book.title + new Date()">
             <td>{{ book.title }}</td>
             <td>{{ book.count }}</td>
-            <td>{{ book.count * Number(book.price) }}</td>
+            <td>{{ book.count * Number(book.price) }} &#3647;</td>
           </tr>
         </tbody>
       </template>
@@ -24,21 +24,73 @@
         <v-list-item-content>
           <v-list-item-title>Discount</v-list-item-title>
         </v-list-item-content>
-        <v-list-item-icon>- {{ totalPrice }}</v-list-item-icon>
+        <v-list-item-icon>- {{ discountPrice }} &#3647;</v-list-item-icon>
       </v-list-item>
       <v-divider />
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title>Total</v-list-item-title>
         </v-list-item-content>
-        <v-list-item-icon>{{ totalPrice }}</v-list-item-icon>
+        <v-list-item-icon>{{ totalPrice }} &#3647;</v-list-item-icon>
       </v-list-item>
       <v-list-item>
         <v-list-item-content>
-          <v-btn @click="Calculate()" dark color="indigo darken-4">Charge</v-btn>
+          <v-text-field v-model="credit" mask="#######" suffix="THB" label="Credit" />
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-btn
+            :disabled="cartList.length <= 0 || credit === ''"
+            @click="Calculate()"
+            color="white--text indigo darken-4"
+          >Charge</v-btn>
         </v-list-item-content>
       </v-list-item>
     </v-list>
+    <v-dialog v-model="dialog" max-width="280">
+      <v-card>
+        <v-card-title v-if="change < 0" class="red--text headline">Not Enough Cash</v-card-title>
+        <v-simple-table v-if="change < 0" height="150px">
+          <template v-slot:default>
+            <tbody>
+              <tr>
+                <td>Credit</td>
+                <td>{{ credit }} &#3647;</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>{{ totalPrice }} &#3647;</td>
+              </tr>
+              <tr class="red--text">
+                <td>Need</td>
+                <td>{{ -change }} &#3647;</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+        <v-card-title v-if="change >= 0" class="green--text headline">
+          Purchase Successful</v-card-title>
+        <v-simple-table v-if="change >= 0" fixed-header height="150px">
+          <template v-slot:default>
+            <tbody>
+              <tr>
+                <td>Credit</td>
+                <td>{{ credit }} &#3647;</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>{{ totalPrice }} &#3647;</td>
+              </tr>
+              <tr>
+                <td>Change</td>
+                <td>{{ change }} &#3647;</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card>
+    </v-dialog>
   </v-navigation-drawer>
 </template>
 
@@ -50,18 +102,42 @@ export default {
   data() {
     return {
       drawer: true,
+      dialog: false,
+      credit: '',
       discountPrice: 0,
-      totalPrice: 0,
     };
   },
   computed: {
     ...mapState('data', {
       cartList: (state) => state.cartList,
     }),
+    totalPrice() {
+      let price = 0;
+      this.cartList.forEach((book) => {
+        price += book.count * Number(book.price);
+      });
+      return price;
+    },
+    change() {
+      return this.credit - this.totalPrice;
+    },
+  },
+  watch: {
+    dialog() {
+      if (this.dialog === false) {
+        if (this.change >= 0) {
+          this.clear();
+        }
+      }
+    },
   },
   methods: {
     Calculate() {
+      this.dialog = true;
+    },
+    clear() {
       this.$store.dispatch('data/clearCart');
+      this.credit = '';
     },
   },
 };
