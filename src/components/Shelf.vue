58283f48cafd62335efee7ff489108ab4d1d7f1e
vuun <vuun.tong@gmail.com>
2020-05-23 14:46:57 +0700

    add pagination

--- src/components/Shelf.vue
+++ src/components/Shelf.vue
@@ -1,22 +1,26 @@
 <template>
   <v-layout wrap>
-    <v-card
-      class="ma-2"
-      v-for="book in bookList"
-      :key="book.title"
-      @click="addBookToCart(book)"
-      width="156"
-    >
-      <v-img :src="`${book.cover}`" aspect-ratio="0.65" class="grey lighten-2">
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular indeterminate color="grey lighten-5" />
-          </v-row>
-        </template>
-      </v-img>
-      <v-card-text>{{ book.title }}</v-card-text>
-      <v-card-text>{{ book.price }} &#3647;</v-card-text>
-    </v-card>
+    <v-pagination v-model="page" :length="shelfLength"></v-pagination>
+    <v-row class="fill-height ma-0 pb-7 mb-4" align="center" justify="center">
+      <v-card
+        align="center"
+        class="mx-2"
+        v-for="book in shelfList"
+        :key="book.title"
+        @click="addBookToCart(book)"
+        width="156"
+      >
+        <v-img :src="`${book.cover}`" aspect-ratio="0.65" class="grey lighten-2">
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular indeterminate color="grey lighten-5" />
+            </v-row>
+          </template>
+        </v-img>
+        <v-card-text>{{ book.title }}</v-card-text>
+        <v-card-text>{{ book.price }} &#3647;</v-card-text>
+      </v-card>
+    </v-row>
   </v-layout>
 </template>
 
@@ -28,10 +32,27 @@ export default {
   data() {
     return {
       bookList: [],
+      shelfList: [],
+      page: 1,
+      bookPerPage: 10,
     };
+  },
+  computed: {
+    shelfLength() {
+      if (this.bookList) {
+        return Math.ceil(this.bookList.length / this.bookPerPage);
+      }
+      return 1;
+    },
   },
   beforeMount() {
     this.getBook();
+  },
+  watch: {
+    page() {
+      this.shelfList = this.bookList.slice((this.page - 1) * this.bookPerPage,
+        (this.page * this.bookPerPage));
+    },
   },
   methods: {
     addBookToCart(book) {
@@ -40,6 +61,8 @@ export default {
     async getBook() {
       const result = await apiBook();
       this.bookList = result.books;
+      this.shelfList = this.bookList.slice((this.page - 1) * this.bookPerPage,
+        (this.page * this.bookPerPage));
       // console.log(this.bookList);
     },
   },
